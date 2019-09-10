73ea990b10c4a038ffd9d2811daf5782ad3c0dbd
Joe Zazzaro-Francis <joez@Gabbana.local>
2019-09-10 11:16:42 +0100

    New branch for frontend UI

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,8 +6,16 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataService {
 
+  roles = [];
   constructor(private http: HttpClient) { }
 
-  
-
+  /*
+  public getAllRoles(): void {
+    this.roles = this.http.post<Roles[]>('/api/roles');
+  }
+  */
+  /*public addCity(newCity: City): void {
+     this.cities = this.http.post<City[]>('/api/addcity', newCity);
+  }x
+  */
 }
