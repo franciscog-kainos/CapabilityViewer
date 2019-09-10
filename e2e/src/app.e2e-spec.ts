b8f5c8fd27d2082e781b7b8953246494cfb85e9e
Clare Buckley <clarebukainos.com@Orchard.kainos.com>
2019-09-10 14:18:10 +0100

    update config, create first title test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,8 +10,10 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to company-app!');
+    expect(page.getTitleText()).toEqual('Welcome to Career Lattice V2.0!');
   });
+
+  
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
