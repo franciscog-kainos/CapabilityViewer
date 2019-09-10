e47fe101a6dccded295d75daf51c40944a0ee153
Joe Zazzaro-Francis <joez@Gabbana.local>
2019-09-10 11:51:07 +0100

    Made typescript objects

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,21 +1,72 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { JobFamily } from './JobFamily';
+import { Capability } from './Capability';
+import { Band } from './Band';
+import { Role } from './Role';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  roles = [];
+  jobFamilies: JobFamily[];
+  capabilities: Capability[];
+  bands: Band[];
+  roles: Role[];
+  role: Role = new Role();
+
   constructor(private http: HttpClient) { }
 
-  /*
-  public getAllRoles(): void {
-    this.roles = this.http.post<Roles[]>('/api/roles');
+  
+  getAllJobFamilies() : void {
+    this.http.get<JobFamily[]>('/api/jobfamilies').subscribe(res => {
+      console.log(res)
+      if(res[0] == null){
+        console.error(res);
+      } else {
+        this.roles = res;
+      }
+    });
   }
-  */
-  /*public addCity(newCity: City): void {
-     this.cities = this.http.post<City[]>('/api/addcity', newCity);
-  }x
-  */
+
+  getAllCapabilities() : void {
+    this.http.get<Capability[]>('/api/capabilities').subscribe(res => {
+      console.log(res)
+      if(res[0] == null){
+        console.error(res);
+      } else {
+        this.roles = res;
+      }
+    });
+  }
+
+  getAllBands() : void {
+    this.http.get<Band[]>('/api/bands').subscribe(res => {
+      console.log(res)
+      if(res[0] == null){
+        console.error(res);
+      } else {
+        this.roles = res;
+      }
+    });
+  }
+
+  getAllRoles() : void {
+    this.http.get<Role[]>('/api/roles').subscribe(res => {
+      console.log(res)
+      if(res[0] == null){
+        console.error(res);
+      } else {
+        this.roles = res;
+      }
+    });
+  }
+
+  getRole(id) : void {
+    this.http.get<Role>('/api/role/' + id).subscribe(role => {
+      this.role = role;
+    });
+  }
 }
