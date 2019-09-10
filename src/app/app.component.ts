e33a027e1b571b9dd8fcd5ce557fa3585949917f
Daniel Kiriasidis Christou <danielch@kainos.com>
2019-09-10 12:02:52 +0100

    Preparation for ag grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,50 @@ import { DataService } from './data.service';
 export class AppComponent {
   title = 'Career Lattice V2.0';
 
+//   columnDefs = [
+//     {headerName: 'Make', field: 'make' },
+//     {headerName: 'Model', field: 'model' },
+//     {headerName: 'Price', field: 'price'}
+// ];
+
+rowData = [
+    { business_development: 'Toyota', model: 'Celica', price: 35000 },
+    { business_development: 'Ford', model: 'Mondeo', price: 32000 },
+    { business_development: 'Porsche', model: 'Boxter', price: 72000 }
+];
+
+
+ columnDefs = [
+  {
+      headerName: 'Sales and Marketing',
+      children: [
+          {headerName: 'Business development', field: 'business_development', width: 150, filter: 'agTextColumnFilter'},
+          {headerName: 'Account Management', field: 'age', width: 90, filter: 'agNumberColumnFilter'},
+          {headerName: 'Sales', field: 'country', width: 120}
+      ]
+  },
+  {
+      headerName: 'Technical',
+      children: [
+          {headerName: 'Software engineer', field: 'sport', width: 90, columnGroupShow: 'open'},
+          {headerName: 'Data Engineering', columnGroupShow: 'open', field: 'total', width: 100, filter: 'agNumberColumnFilter'},
+      ]
+  }
+];
+
+ gridOptions = {
+  defaultColDef: {
+      sortable: true,
+      resizable: true,
+      filter: true
+  },
+  debug: true,
+  columnDefs: this.columnDefs,
+  rowData: this.rowData
+};
+
+
+
+
   constructor () { }
 }
