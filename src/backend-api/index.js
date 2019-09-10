c78128661a7017b2819cc128792ce233ecab5830
Alex Owen-Meehan <alexo@tribore.local>
2019-09-10 10:14:40 +0100

    Added basic express layer and more into gitignore

--- src/backend-api/index.js
+++ src/backend-api/index.js
@@ -1,10 +1,35 @@
 const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-app.listen(8002, function() {
-  console.log('World API listening on port 8002');
-});
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const db = require('./db.js');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+function handleError(err, req, res) {
+    if (err.errno === 3819) err.code = "ER_CHECK_CONSTRAINT_VIOLATED";
+    console.error(`${err.errno} (${err.code}) : ${err.sqlMessage}`);
+    res.status(500).send({
+        message: 'Database error. ' + err.sqlMessage
+    });
+}
+
+app.get('/', function(req, res) {
+        res.send(200);
+    });
+
+app.get('/roles', function(req, res) {
+    db.getAllRoles(req.body, function(err) {
+        if (err) return handleError(err, req, res);
+        res.send(200);
+    });
+});
+
+app.listen(8002, function() {
+    console.log('Server listening on port 8002');
+});
