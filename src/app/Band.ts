86eaf6d271aaa90cd9b0cb7c7fe2d81fa84bd362
Joe <zazz.joe@hotmail.com>
2019-09-10 14:09:54 +0100

    Ds002/ui (#7)
    * Added functions for communicating with express layer
    
    * Fixed typescript objects

--- src/app/Band.ts
+++ src/app/Band.ts
@@ -1,3 +1,2 @@
-export class Role {
-    
+export class Band {
 }
--- src/app/Role.ts
+++ src/app/Role.ts
@@ -1,3 +1,9 @@
 export class Role {
-    
+    role_id: number;
+    role_name: string;
+    role_summary: string;
+    role_training: string;
+    role_responsibilities: string;
+    capability_id: number;
+    band_id: number;
 }
