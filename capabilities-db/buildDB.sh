7af21042ec0262e423b1d0b2e61e00cf5e73b1c7
saddiqs1 <ssaddique96@hotmail.com>
2019-09-11 08:58:56 +0100

    Update buildDB.sh

--- capabilities-db/buildDB.sh
+++ capabilities-db/buildDB.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysql -u root -p -e "source capabilitiesDB-setup_prod.sql; source capabilitiesDB-setup_test.sql; source capabilitiesDB-populate.sql;"
+mysql -u root -p -e "source capabilitiesDB-setup_prod.sql; use capabilitiesDB_prod; source capabilitiesDB-populate.sql; source capabilitiesDB-setup_test.sql;  use capabilitiesDB_test; source capabilitiesDB-populate.sql;"
