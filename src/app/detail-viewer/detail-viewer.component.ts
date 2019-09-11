1db7d39e3d833378bc482e5ca1ca3d59551d1f69
Shuaib Siddique <shuaibsiddique@Viara.kainos.com>
2019-09-11 16:59:12 +0100

    refactored routing to make typescript navigate method work

--- src/app/detail-viewer/detail-viewer.component.ts
+++ src/app/detail-viewer/detail-viewer.component.ts
@@ -1,38 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from "@angular/common";
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { DataService } from '../data.service';
 import { Band } from '../Band';
 import { Role } from '../Role';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'detail-viewer',
-  templateUrl: './detail-viewer.component.html',
-  styleUrls: ['./detail-viewer.component.css']
+    selector: 'detail-viewer',
+    templateUrl: './detail-viewer.component.html',
+    styleUrls: ['./detail-viewer.component.css']
 })
 export class DetailViewerComponent implements OnInit {
-   data: DataService;
-   band: Band;
-   role: Role;
+    data: DataService;
+    band: Band;
+    role: Role;
+    role$: Observable<Role>;
 
-  constructor(private location: Location, data: DataService) {
-      data.getBandDetails(1).subscribe(response => {
-          this.band = response;
-          console.log(this.band);
-      });
+    constructor(private location: Location, data: DataService, private route: ActivatedRoute, private router: Router) {
+        data.getBandDetails(1).subscribe(response => {
+            this.band = response;
+            console.log(this.band);
+        });
 
-      data.getRole(1).subscribe(response => {
-          this.role = response;
-          console.log(this.role);
-      });
-  }
+        data.getRole(1).subscribe(response => {
+            this.role = response;
+            console.log(this.role);
+        });
+    }
 
-  ngOnInit() {
-    this.band = new Band();
-    this.role = new Role();
-  }
+    ngOnInit() {
+        this.band = new Band();
 
-  goBack() {
-    //TODO - change the way this navigates back
-    this.location.back();
-  }
+        //let id = this.route.snapshot.paramMap.get('id');
+        //this.role = this.data.getRole(id);
+        this.role$ = this.route.paramMap.pipe(
+            switchMap((params: ParamMap) =>
+                this.data.getRole(params.get('id')))
+        );
+    }
+
+    goBack() {
+        //TODO - change the way this navigates back
+        this.router.navigate(['/table-page']);
+    }
 }
