6cbc0a534b5c53424671a64171c8f2220801315d
Daniel Kiriasidis Christou <danielch@kainos.com>
2019-09-12 10:10:32 +0100

    Add algorithm and integrate with on click

--- src/app/table-page/table-page.component.ts
+++ src/app/table-page/table-page.component.ts
@@ -48,28 +48,71 @@ export class TablePageComponent implements OnInit {
     this.location.back();
   }
 
-  generateTable(jobFamilies: JobFamily[], capabilities: Capability[], bands: Band[], roles: Role[]) {
-    this.rowData = [
-      { business_development: 'Toyota', model: 'Celica', price: 35000 },
-      { business_development: 'Ford', model: 'Mondeo', price: 32000 },
-      { business_development: 'Porsche', model: 'Boxter', price: 72000 }
-    ];
+  generateTable(jobFamilies: JobFamily[], capabilities: Capability[], bands2: Band[], roles: Role[]) {
+    this.rowData = [];
+   
+    var currentBandId = 1; //bands start at one
 
+    var rowToAppend = []; 
+    var columnsToShow = ["firstColumn","secondColumn","thirdColumn","fourthColumn"]; 
+
+    var j = 0; 
+    var bands = ["Executive","Principal","Manager","Senior","Associate"]; //Bands to display on the front end.
+    
+    var k = 0;
+    rowToAppend["bandLevels"] = bands[k]; //The very first item in the row should be the band level.
+
+    var i;
+    for(i = 0; i < roles.length; i++){ //for each role
+      if (currentBandId == roles[i].band_id){
+      //we are in the current band, object should be appended to with new roles    
+      rowToAppend[columnsToShow[j]]= roles[i].role_name; //Here we are building up the row with the new role
+
+      } else {
+        // band has changed, we should push the new band and create a new one.
+
+        rowToAppend["bandLevels"]= bands[k];  //add band level for new row
+        k++; 
+        
+        this.rowData.push(rowToAppend); //push the completed band row
+
+        rowToAppend = []; //empty the row that was being built up
+        j = 0; //reset the counter which keeps track of which columns should be shown (so that it starts building the new row from column 0)
+        
+        //Add a role to the new band with 
+        rowToAppend[columnsToShow[j]]= roles[i].role_name;
+      }
+     j++; 
+     currentBandId = roles[i].band_id; // update what band we are in
+    }
+
+    rowToAppend["bandLevels"]= bands[k];  
+    this.rowData.push(rowToAppend);
+    // When the loop has finished, push the final row it was building up. 
+
+    console.log("Full data \n" );
 
     this.columnDefs = [
       {
-        headerName: jobFamilies[0].job_family_name,
+        headerName: "Band Level",
         children: [
-          { headerName: capabilities[0].capability_name, field: 'business_development', width: 150, filter: 'agTextColumnFilter' },
-          { headerName: capabilities[1].capability_name, field: 'age', width: 90, filter: 'agNumberColumnFilter' },
-          { headerName: 'Sales', field: 'country', width: 120 }
+          { headerName: '', field: 'bandLevels', width: 0, filter: 'agTextColumnFilter' },
         ]
       },
       {
-        headerName: jobFamilies[1].job_family_name,
+        headerName: "Sales and Marketing",
         children: [
-          { headerName: 'Software engineer', field: 'sport', width: 90, columnGroupShow: 'open' },
-          { headerName: 'Data Engineering', columnGroupShow: 'open', field: 'total', width: 100, filter: 'agNumberColumnFilter' },
+          { headerName: 'Business development', field: 'firstColumn', width: 200, filter: 'agTextColumnFilter' },
+          { headerName: 'Account Management', field: 'secondColumn', width: 200, filter: 'agNumberColumnFilter' },
+          { headerName: 'Sales', field: 'thirdColumn', width: 200 },
+          { headerName: 'Sales', field: 'fourthColumn', width: 200 }
+        ]
+      },
+      {
+        headerName: "Technical",
+        children: [
+          { headerName: 'Software engineer', field: 'sda', width: 200, columnGroupShow: 'open' },
+          { headerName: 'Data Engineering', columnGroupShow: 'open', field: 'total', width: 200, filter: 'agNumberColumnFilter' },
         ]
       }
     ];
@@ -84,8 +127,8 @@ export class TablePageComponent implements OnInit {
       columnDefs: this.columnDefs,
       rowData: this.rowData,
     };
-
   }
+  
 
   onCellClicked(event){
     var focusedCell = this.gridOptions.api.getFocusedCell();
