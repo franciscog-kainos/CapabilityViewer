51cf3105ef851b8f579105909c95465c5742c063
Francisco Goinhas <f.goinhas@kainos.com>
2019-09-13 09:59:13 +0100

    User changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common'
-import { AgGridModule } from 'ag-grid-angular';
-import { PageHeaderComponent } from './page-header/page-header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { AppRoutingModule } from './app-routing.module';
-import {LandingPageComponent} from "./landing-page/landing-page.component";
-import {TablePageComponent} from "./table-page/table-page.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-import {DetailViewerComponent} from "./detail-viewer/detail-viewer.component";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from '@angular/common/http';
+import {CommonModule} from '@angular/common';
+import {AgGridModule} from 'ag-grid-angular';
+import {PageHeaderComponent} from './page-header/page-header.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {AppRoutingModule} from './app-routing.module';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {TablePageComponent} from './table-page/table-page.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {DetailViewerComponent} from './detail-viewer/detail-viewer.component';
 import {MatCardModule} from '@angular/material';
+import {CapabilityLeadViewerComponent} from './capability-lead-viewer/capability-lead-viewer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PageHeaderComponent,
-    LandingPageComponent,
-    TablePageComponent,
-    DetailViewerComponent,
-    PageNotFoundComponent,
-    CapabilityLeadViewerComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    NgbModule,
-    HttpClientModule,
-    AgGridModule.withComponents([]),
-    BrowserAnimationsModule,
-    MatExpansionModule,
-    AppRoutingModule,
-      MatCardModule
-  ],
-  providers: [],
-  bootstrap: [ AppComponent ]
+    declarations: [
+        AppComponent,
+        PageHeaderComponent,
+        LandingPageComponent,
+        TablePageComponent,
+        DetailViewerComponent,
+        PageNotFoundComponent,
+        CapabilityLeadViewerComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NgbModule,
+        HttpClientModule,
+        AgGridModule.withComponents([]),
+        BrowserAnimationsModule,
+        MatExpansionModule,
+        AppRoutingModule,
+        MatCardModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+}
