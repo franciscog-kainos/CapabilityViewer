ae754bff1d6c787684419d43cb8dd89518d17a66
Sifat Jahir <sifatjahir@hotmail.com>
2019-09-17 11:30:17 +0100

    Created protractor tests for filter by job family

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { LandingPage } from './landing-page.po';
 import { browser, logging } from 'protractor';
-
-describe('Landing Page', () => {
+import { TablePage } from './table-page.po';
+/*describe('Landing Page', () => {
   let page: LandingPage;
 
   beforeEach(() => {
@@ -26,4 +26,39 @@ describe('Landing Page', () => {
       level: logging.Level.SEVERE,
     } as logging.Entry));
   });
+});*/
+
+
+describe('Table Page', () => {
+  let page: TablePage;
+
+  beforeEach(() => {
+    page = new TablePage();
+  });
+
+  it('should display column header', () => {
+    page.navigateTo();
+    expect(page.getColumnHeaderText()).toEqual('Sales and Marketing');
+  });
+
+  it('should expand the column when the arrow icon is clicked', () => {
+    let elem = page.getColumnExpandIcon();
+    page.clickColumnExpandIcon();
+    //expect(browser.getCurrentUrl()).toEqual(browser.baseUrl + "table-page");
+    expect(elem.getAttribute('class')).toContain("ag-hidden");
+
+  });
+
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
 });
+
+
+
+
